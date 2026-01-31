06955e68944db3995caca0864ae6ce2c800048d4
Radja Thaher <radjapolem@gmail.com>
2026-01-31 12:02:50 +0700

    chore: update homebrew sha

--- Formula/cloudflare-cli.rb
+++ Formula/cloudflare-cli.rb
@@ -7,7 +7,7 @@ class CloudflareCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/radjathaher/cloudflare-cli/releases/download/v0.1.0/cloudflare-cli-0.1.0-darwin-aarch64.tar.gz"
-      sha256 "TODO"
+      sha256 "f966a29c56e2773c0047d9d28004568ad0f9c68ff1a9143ac0ed8d2f015701e0"
     else
       odie "cloudflare-cli is only packaged for macOS arm64"
     end
